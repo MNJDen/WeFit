4920887c2b96b9ca78e990b59bd7b55a975b6a5d
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-04-12 09:39:22 +0800

    Pages: Add Track Screen

---
+++ lib/Screens/TrackScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TrackScreen extends StatefulWidget {
+  const TrackScreen({super.key});
+
+  @override
+  State<TrackScreen> createState() => _TrackScreenState();
+}
+
+class _TrackScreenState extends State<TrackScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

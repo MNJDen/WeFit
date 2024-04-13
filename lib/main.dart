643945f1c5249de92568d1eddda0c226961a416b
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-04-13 19:43:17 +0800

    Pages: Update Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:itec303/Services/Auth/Auth_Gate.dart';
 import 'package:itec303/Components/MyUsernameField.dart';
-import 'package:itec303/Screens/SignInScreen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:itec303/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(name: "WeFit", options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -22,8 +26,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             fontFamily: 'General Sans',
           ).copyWith(
-            colorScheme:
-                ThemeData().colorScheme.copyWith(primary: purpleColor),
+            colorScheme: ThemeData().colorScheme.copyWith(primary: purpleColor),
           ),
           home: const RootPage(),
         );
@@ -43,7 +46,7 @@ class _RootPageState extends State<RootPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: SignInScreen(),
+      body: AuthGate(),
     );
   }
 }

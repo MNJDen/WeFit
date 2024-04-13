0ee7168187ba29be3e8ae1ff809955de62cd9692
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-04-13 19:52:55 +0800

    Pages: Update Chat Screen

--- lib/Screens/ChatScreen.dart
+++ lib/Screens/ChatScreen.dart
@@ -1,15 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:itec303/Components/MyPasswordField.dart';
+import 'package:itec303/Components/User_Tile.dart';
+import 'package:itec303/Screens/ChatPage.dart';
+import 'package:itec303/Services/Auth/Auth_Service.dart';
+import 'package:itec303/Services/Chat/Chat_Service.dart';
 
-class ChatScreen extends StatefulWidget {
-  const ChatScreen({super.key});
+class ChatScreen extends StatelessWidget {
+  ChatScreen({super.key});
 
-  @override
-  State<ChatScreen> createState() => _ChatScreenState();
-}
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
 
-class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: blackColor,
+      appBar: AppBar(
+        backgroundColor: blackColor,
+        title: Text(
+          "Chats",
+          style: TextStyle(
+            fontSize: 20.sp,
+            fontWeight: FontWeight.w500,
+            color: whiteColor,
+          ),
+        ),
+      ),
+      body: _buildUserList(),
+    );
+  }
+
+  Widget _buildUserList() {
+    return StreamBuilder(
+      stream: _chatService.getUsersStream(),
+      builder: (context, snapshot) {
+        //error
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        //loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading...");
+        }
+
+        return ListView(
+          children: snapshot.data!
+              .map<Widget>((userData) => _buildUserListItem(userData, context))
+              .toList(),
+        );
+      },
+    );
+  }
+
+  Widget _buildUserListItem(
+      Map<String, dynamic> userData, BuildContext context) {
+    if (userData["email"] != _authService.getCurrentUser()!.email) {
+      return UserTile(
+        text: userData["email"],
+        onTap: () {
+          Navigator.push(
+            context,
+            PageRouteBuilder(
+              pageBuilder: (BuildContext context, Animation<double> animation1,
+                  Animation<double> animation2) {
+                return ChatPage(
+                  receiverEmail: userData["email"],
+                  receiverID: userData["uid"],
+                );
+              },
+              transitionDuration: Duration.zero,
+              reverseTransitionDuration: Duration.zero,
+            ),
+          );
+        },
+      );
+    } else {
+      return Container();
+    }
   }
 }

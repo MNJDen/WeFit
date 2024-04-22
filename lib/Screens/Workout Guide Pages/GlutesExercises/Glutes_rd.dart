8706be436753fa808d40d5d0c3a11452b7cebb02
mathew <90034902+mathewwww@users.noreply.github.com>
2024-04-22 21:37:26 +0800

    Pages: Add Glutes_rd Glutes

---
+++ lib/Screens/Workout Guide Pages/GlutesExercises/Glutes_rd.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:itec303/Components/MyUsernameField.dart';
+
+class Glutes_rd extends StatefulWidget {
+  const Glutes_rd({super.key});
+
+  @override
+  State<Glutes_rd> createState() => _Glutes_rdState();
+}
+
+class _Glutes_rdState extends State<Glutes_rd> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: blackColor,
+      appBar: AppBar(
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Back",
+              style: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w400,
+                color: whiteColor,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_circle_left_rounded,
+            color: purpleColor,
+            size: 30,
+          ),
+        ),
+        leadingWidth: 30.w,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20.h,
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      '',
+                      height: 150.h, // Adjust the height as needed
+                      width: 320.w, // Adjust the width as needed
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 17.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Romanian Deadlift",
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w500,
+                        color: whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 4.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Primary Muscle Group: Glutes",
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w200,
+                        color: whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 12.h,
+                ),
+                Text(
+                  "1. Starting position is standing straight with your feet hip-width apart, clutching a pair of dumbbells at the front of your thighs, with your palms directed towards your body.\n"
+                  "2. From here, keep your back straight and bend at the hips. Put a slight bend in the knees – just enough to relieve tension off them.\n"
+                  "3. Lower the pair of weights towards the floor, ensuring they remain close to your shins. Lower until you feel a stretch in your hamstrings, or the dumbbells are just above the ground, depending on your flexibility.\n"
+                  "4. Engage your glutes and hamstrings to push your hips forward and return to standing position, while lifting the dumbbells back to the starting position.\n"
+                  "5. Be sure to keep your core engaged throughout the movement to maintain a neutral position in the spine.\n",
+                  style: TextStyle(
+                    fontSize: 12.sp,
+                    fontWeight: FontWeight.w200,
+                    color: whiteColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

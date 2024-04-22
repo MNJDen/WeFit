0626675afa57619fc1dc4387ccedf4f61706f6d8
mathew <90034902+mathewwww@users.noreply.github.com>
2024-04-22 21:41:59 +0800

    Pages: Add Shoulders_op Shoulders

---
+++ lib/Screens/Workout Guide Pages/ShoulderExercises/Shoulders_op.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:itec303/Components/MyUsernameField.dart';
+
+class Shoulders_op extends StatefulWidget {
+  const Shoulders_op({super.key});
+
+  @override
+  State<Shoulders_op> createState() => _Shoulders_opState();
+}
+
+class _Shoulders_opState extends State<Shoulders_op> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: blackColor,
+      appBar: AppBar(
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Back",
+              style: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w400,
+                color: whiteColor,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_circle_left_rounded,
+            color: purpleColor,
+            size: 30,
+          ),
+        ),
+        leadingWidth: 30.w,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20.h,
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      '',
+                      height: 150.h, // Adjust the height as needed
+                      width: 320.w, // Adjust the width as needed
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 17.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Overhead Press",
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w500,
+                        color: whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 4.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Primary Muscle Group: Shoulders",
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w200,
+                        color: whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 12.h,
+                ),
+                Text(
+                  "1. Stand with the bar on your front shoulders, and your hands next to your shoulders.\n"
+                  "2. Press the bar over your head, until it’s balanced over your shoulders and mid-foot.\n"
+                  "3. Lock your elbows at the top, and shrug your shoulders to the ceiling.\n",
+                  style: TextStyle(
+                    fontSize: 12.sp,
+                    fontWeight: FontWeight.w200,
+                    color: whiteColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

4fcf72a4065ba8ff5bf502c7f8282516e275c053
mathew <90034902+mathewwww@users.noreply.github.com>
2024-04-22 21:42:19 +0800

    Pages: Add Shoulders_slr Shoulders

---
+++ lib/Screens/Workout Guide Pages/ShoulderExercises/Shoulders_slr.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:itec303/Components/MyUsernameField.dart';
+
+class Shoulders_slr extends StatefulWidget {
+  const Shoulders_slr({super.key});
+
+  @override
+  State<Shoulders_slr> createState() => _Shoulders_slrState();
+}
+
+class _Shoulders_slrState extends State<Shoulders_slr> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: blackColor,
+      appBar: AppBar(
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Back",
+              style: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w400,
+                color: whiteColor,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_circle_left_rounded,
+            color: purpleColor,
+            size: 30,
+          ),
+        ),
+        leadingWidth: 30.w,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20.h,
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      '',
+                      height: 150.h, // Adjust the height as needed
+                      width: 320.w, // Adjust the width as needed
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 17.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Side Lateral Raise",
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w500,
+                        color: whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 4.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Primary Muscle Group: Shoulders",
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w200,
+                        color: whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 12.h,
+                ),
+                Text(
+                  "1. Grab two dumbbells and stand with your feet hip-width apart with a slight bend in your knees. Your posture should be tall with your hips slightly bent forward with a neutral head and neck position. Your shoulders should be slightly ahead of your hips.\n"
+                  "2. Pretension your shoulders and hips while engaging your core.\n"
+                  "3. Initiate the upward movement by slowly lifting your arms away from your body. Allow the shoulder blades to move naturally with the shoulder joint. Continue to raise your arms until your elbows are at shoulder height or just below your shoulders. You should still feel tension in your shoulders. Your palms should be facing the floor at the top of the movement.\n"
+                  "4. Pause at the top of the movement.\n",
+                  style: TextStyle(
+                    fontSize: 12.sp,
+                    fontWeight: FontWeight.w200,
+                    color: whiteColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

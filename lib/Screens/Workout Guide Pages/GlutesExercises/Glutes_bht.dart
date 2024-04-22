acabfc61b6c8e040eb147c9537e44a3f533264f2
mathew <90034902+mathewwww@users.noreply.github.com>
2024-04-22 21:36:40 +0800

    Pages: Add Glutes_bht Glutes

---
+++ lib/Screens/Workout Guide Pages/GlutesExercises/Glutes_bht.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:itec303/Components/MyUsernameField.dart';
+
+class Glutes_bht extends StatefulWidget {
+  const Glutes_bht({super.key});
+
+  @override
+  State<Glutes_bht> createState() => _Glutes_bhtState();
+}
+
+class _Glutes_bhtState extends State<Glutes_bht> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: blackColor,
+      appBar: AppBar(
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Back",
+              style: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w400,
+                color: whiteColor,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_circle_left_rounded,
+            color: purpleColor,
+            size: 30,
+          ),
+        ),
+        leadingWidth: 30.w,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20.h,
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      '',
+                      height: 150.h, // Adjust the height as needed
+                      width: 320.w, // Adjust the width as needed
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 17.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Barbell Hip Thrust",
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w500,
+                        color: whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 4.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Primary Muscle Group: Glutes",
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w200,
+                        color: whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 12.h,
+                ),
+                Text(
+                  "1. The proper position for the hip thrust is on the floor with a bench situated right behind you and a loaded barbell resting over your legs, hands holding onto it with an overhand grip, feet under hips.\n"
+                  "2. If you’re worried about the floor, you can use a barbell with a rubber bumper. Ensure the bar is rolled so that it aligns directly above your hips.\n"
+                  "3. Lean backwards against the bench, placing your shoulder blades near the upper edge of the bench.\n"
+                  "4. Begin by driving through your feet and extending your hips vertically, lifting the barbell up.\n"
+                  "5. Once your glutes are locked out, slowly reverse the motion, bringing your hips back down to return to the initial position.\n"
+                  "6. Throughout this exercise, it’s crucial to keep your weight on your heels and maintain a neutral spine to avoid straining your lower back or neck. The goal is to perform this exercise for glute growth in a slow, controlled manner, prioritizing proper form over lifting heavy weights, because using proper posture ensures less risk of injury.\n",
+                  style: TextStyle(
+                    fontSize: 12.sp,
+                    fontWeight: FontWeight.w200,
+                    color: whiteColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

994a56f0be14cdf7cbc16d578ccf35e3f9ae2362
mathew <90034902+mathewwww@users.noreply.github.com>
2024-04-22 21:33:20 +0800

    Pages: Add Arms_ wc Arms

---
+++ lib/Screens/Workout Guide Pages/ArmExercises/Arms_wc.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:itec303/Components/MyUsernameField.dart';
+
+class Arms_wc extends StatefulWidget {
+  const Arms_wc({super.key});
+
+  @override
+  State<Arms_wc> createState() => _Arms_wcState();
+}
+
+class _Arms_wcState extends State<Arms_wc> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: blackColor,
+      appBar: AppBar(
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Back",
+              style: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w400,
+                color: whiteColor,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_circle_left_rounded,
+            color: purpleColor,
+            size: 30,
+          ),
+        ),
+        leadingWidth: 30.w,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20.h,
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      '',
+                      height: 150.h, // Adjust the height as needed
+                      width: 320.w, // Adjust the width as needed
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 17.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Wrist Curl",
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w500,
+                        color: whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 4.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Primary Muscle Group: Arms",
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w200,
+                        color: whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 12.h,
+                ),
+                Text(
+                  "1. Curl your wrists upward as you exhale. Perform this movement slowly to engage the forearms and prevent the weights from straining your wrists.\n"
+                  "2. Extend your wrists downward as far as they will comfortably go on an inhale.\n"
+                  "3. Return the wrists to a neutral position.\n",
+                  style: TextStyle(
+                    fontSize: 12.sp,
+                    fontWeight: FontWeight.w200,
+                    color: whiteColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

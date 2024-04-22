d587dd8105e5a3633c06c0061c3cfd5410d78213
mathew <90034902+mathewwww@users.noreply.github.com>
2024-04-22 21:35:26 +0800

    Pages: Add Chest_dfbf Chest

---
+++ lib/Screens/Workout Guide Pages/ChestExercises/Chest_dfbf.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:itec303/Components/MyUsernameField.dart';
+
+class Chest_dfbf extends StatefulWidget {
+  const Chest_dfbf({super.key});
+
+  @override
+  State<Chest_dfbf> createState() => _Chest_dfbfState();
+}
+
+class _Chest_dfbfState extends State<Chest_dfbf> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: blackColor,
+      appBar: AppBar(
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Back",
+              style: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w400,
+                color: whiteColor,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_circle_left_rounded,
+            color: purpleColor,
+            size: 30,
+          ),
+        ),
+        leadingWidth: 30.w,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20.h,
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      '',
+                      height: 150.h, // Adjust the height as needed
+                      width: 320.w, // Adjust the width as needed
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 17.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Dumbbell Flat Bench Flyes",
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w500,
+                        color: whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 4.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Primary Muscle Group: Chest",
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w200,
+                        color: whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 12.h,
+                ),
+                Text(
+                  "1. Start by lying on a bench with the dumbbells extended so that your hands are stacked directly over your shoulders. Turn your palms in so they face each other. Keep your shoulder blades squeezed and in contact with the bench for the entire duration of the movement.\n"
+                  "2. Bend your elbows slightly and keep them at a consistent angle throughout the movement. Then, slowly lower the dumbbells out to the side until your arms are parallel with the floor and your pecs are significantly stretched. Pause at the bottom briefly.\n"
+                  "3. From the bottom position, smoothly reverse the motion. Maintain that same slight bend in your elbows as you return to the starting position.\n",
+                  style: TextStyle(
+                    fontSize: 12.sp,
+                    fontWeight: FontWeight.w200,
+                    color: whiteColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

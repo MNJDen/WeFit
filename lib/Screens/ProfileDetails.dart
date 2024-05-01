6933171b0b15fcb0575c487b6a82d18c525ec5ec
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-05-01 19:45:13 +0800

    Pages: Add Profile Details

---
+++ lib/Screens/ProfileDetails.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileDetailsScreen extends StatefulWidget {
+  const ProfileDetailsScreen({super.key});
+
+  @override
+  State<ProfileDetailsScreen> createState() => _ProfileDetailsScreenState();
+}
+
+class _ProfileDetailsScreenState extends State<ProfileDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

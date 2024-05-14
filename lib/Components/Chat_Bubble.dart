d406a63be27912661adb0a16f800f1f3992cc5b5
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-05-14 18:30:01 +0800

    Components: Update Chat Bubble

--- lib/Components/Chat_Bubble.dart
+++ lib/Components/Chat_Bubble.dart
@@ -18,7 +18,7 @@ class ChatBubble extends StatelessWidget {
       margin: isCurrentUser ? EdgeInsets.only(left: 50.w) : EdgeInsets.only(right: 15.w),
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 15.h, vertical: 8.h),
-        margin: EdgeInsets.symmetric(horizontal: 15.h, vertical: 5.h),
+        margin: EdgeInsets.symmetric(horizontal: 15.h, vertical: 1.h),
         decoration: BoxDecoration(
           color: isCurrentUser ? purpleColor : Color.fromRGBO(59, 59, 59, 0.298),
           borderRadius: BorderRadius.circular(10),

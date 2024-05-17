d7ae83598a92fbbdc7500643e502afb9ece4132d
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-05-17 16:51:15 +0800

    Pages: Update Auth Service

--- lib/Services/Auth/Auth_Service.dart
+++ lib/Services/Auth/Auth_Service.dart
@@ -4,16 +4,38 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'dart:io';
 
 class AuthService {
-  //instance of auth & firestore
+  // Instance of auth & firestore
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  //get current user
+  // Get current user
   User? getCurrentUser() {
     return _auth.currentUser;
   }
 
-// sign in
+  // Error Messages
+  String getErrorMessage(String errorCode) {
+    switch (errorCode) {
+      case 'invalid-email':
+        return 'The email address is not valid.';
+      case 'user-disabled':
+        return 'This user has been disabled.';
+      case 'user-not-found':
+        return 'No user found with this email.';
+      case 'wrong-password':
+        return 'Incorrect password. Please try again.';
+      case 'email-already-in-use':
+        return 'The email address is already in use.';
+      case 'operation-not-allowed':
+        return 'Email/password accounts are not enabled.';
+      case 'weak-password':
+        return 'The password is too weak.';
+      default:
+        return 'An unknown error occurred. Please try again later.';
+    }
+  }
+
+  // Sign In
   Future<UserCredential?> signInWithEmailPassword(
       String email, String password, String username) async {
     try {
@@ -30,16 +52,16 @@ class AuthService {
 
       // Check if the username exists in Firestore
       if (!userDoc.exists) {
-        throw Exception("User data not found in Firestore");
+        throw AuthServiceException("User data not found in Firestore");
       }
 
       return userCredential;
     } on FirebaseAuthException catch (e) {
-      throw Exception(e.code);
+      throw AuthServiceException(getErrorMessage(e.code));
     }
   }
 
-// sign up
+  // Sign Up
   Future<UserCredential?> signUpWithEmailPassword(
       String email, String password, String username, File image) async {
     try {
@@ -62,13 +84,13 @@ class AuthService {
           'uid': userCredential.user!.uid,
           'email': email,
           'username': username,
-          'profileImageUrl': imageUrl, 
+          'profileImageUrl': imageUrl,
         },
       );
 
       return userCredential;
     } on FirebaseAuthException catch (e) {
-      throw Exception(e.code);
+      throw AuthServiceException(getErrorMessage(e.code));
     }
   }
 
@@ -94,8 +116,16 @@ class AuthService {
     }
   }
 
-// sign out
+  // Sign out
   Future<void> signOut() async {
     return await _auth.signOut();
   }
+}
+
+class AuthServiceException implements Exception {
+  final String message;
+  AuthServiceException(this.message);
+
+  @override
+  String toString() => message; // Override toString to return just the message
 }

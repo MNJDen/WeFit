836d32b7c92b3e2d23f64fc5149c64adfa058e04
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-05-17 14:50:44 +0800

    Pages: Update Abs

--- lib/Screens/Workout Guide Pages/AbsExercises/Abs_cc.dart
+++ lib/Screens/Workout Guide Pages/AbsExercises/Abs_cc.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:itec303/Components/MyUsernameField.dart';
 
@@ -61,7 +62,7 @@ class _Abs_ccState extends State<Abs_cc> {
                       fit: BoxFit.cover,
                     ),
                   ],
-                ),
+                ).animate().fadeIn(delay: const Duration(milliseconds: 400)),
                 SizedBox(
                   height: 17.h,
                 ),
@@ -76,7 +77,7 @@ class _Abs_ccState extends State<Abs_cc> {
                       ),
                     ),
                   ],
-                ),
+                ).animate().fadeIn(delay: const Duration(milliseconds: 500)),
                 SizedBox(
                   height: 4.h,
                 ),
@@ -91,7 +92,7 @@ class _Abs_ccState extends State<Abs_cc> {
                       ),
                     ),
                   ],
-                ),
+                ).animate().fadeIn(delay: const Duration(milliseconds: 600)),
                 SizedBox(
                   height: 12.h,
                 ),
@@ -107,12 +108,12 @@ class _Abs_ccState extends State<Abs_cc> {
                     fontWeight: FontWeight.w200,
                     color: whiteColor,
                   ),
-                ),
+                ).animate().fadeIn(delay: const Duration(milliseconds: 700)),
               ],
             ),
           ),
         ),
       ),
-    );
+    ).animate().fadeIn(delay: const Duration(milliseconds: 300));
   }
 }

0c3a701b28cf44ff68b12220ba8c6055f231e46f
“jlexm” <jamaravilla@gbox.adnu.edu.ph>
2024-05-18 19:31:40 +0800

    Models: Update Exercise Item

--- lib/Models/exercise_item.dart
+++ lib/Models/exercise_item.dart
@@ -2,10 +2,14 @@ class ExerciseItem {
   String name;
   String imagePath;
   String id;
+  bool isNeedWeights;
+  bool isNeedTimer;
 
-  ExerciseItem({
-    required this.name,
-    required this.imagePath,
-    required this.id,
-  });
+  ExerciseItem(
+      {required this.name,
+      required this.imagePath,
+      required this.id,
+      this.isNeedWeights = true,
+      this.isNeedTimer = false,
+      });
 }

db8620f7aec5c25ad630e301031bd9ce8c557230
“jlexm” <jamaravilla@gbox.adnu.edu.ph>
2024-05-18 19:33:32 +0800

    Pages: Relocate Tracke Save

---
+++ lib/Screens/TrackerPages/TrackSave.dart
@@ -0,0 +1,329 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:itec303/Components/MyPasswordField.dart';
+import 'package:itec303/Models/exercise_item.dart';
+import 'package:itec303/Screens/TrackScreen.dart';
+
+class TrackSave extends StatefulWidget {
+  final Function(String, String, String)? addExercise;
+  final ExerciseItem exercise;
+  final String categoryName;
+  final int? initialSets;
+  final int? initialReps;
+  final double? initialWeight;
+  final int? initialMins;
+
+  const TrackSave({
+    Key? key,
+    this.addExercise,
+    required this.exercise,
+    required this.categoryName,
+    this.initialSets,
+    this.initialReps,
+    this.initialWeight,
+    this.initialMins,
+  }) : super(key: key);
+
+  @override
+  State<TrackSave> createState() => _TrackSaveState();
+}
+
+class _TrackSaveState extends State<TrackSave> {
+  late TextEditingController setsController;
+  late TextEditingController repsController;
+  late TextEditingController weightController;
+  late TextEditingController minutesController;
+
+  @override
+  void initState() {
+    super.initState();
+    setsController = TextEditingController(text: widget.initialSets.toString());
+    repsController = TextEditingController(text: widget.initialReps.toString());
+    weightController =
+        TextEditingController(text: widget.initialWeight.toString());
+    minutesController =
+        TextEditingController(text: widget.initialWeight.toString());
+  }
+
+  void increment(TextEditingController controller) {
+    int value = int.tryParse(controller.text) ?? 0;
+    setState(() {
+      controller.text = (value + 1).toString();
+    });
+  }
+
+  void decrement(TextEditingController controller) {
+    int value = int.tryParse(controller.text) ?? 0;
+    if (value > 0) {
+      setState(() {
+        controller.text = (value - 1).toString();
+      });
+    }
+  }
+
+  Future<void> handleSave() async {
+    int numSets = int.tryParse(setsController.text) ?? 0;
+    int numReps = int.tryParse(repsController.text) ?? 0;
+    double numWeights = double.tryParse(weightController.text) ?? 0.0;
+    int mins = int.tryParse(minutesController.text) ?? 0;
+    String muscleGroup = widget.categoryName;
+
+    await _saveExerciseProgressToFirestore(
+      numSets,
+      numReps,
+      numWeights,
+      mins,
+      muscleGroup,
+    );
+
+    Navigator.push(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (BuildContext context, Animation<double> animation1,
+            Animation<double> animation2) {
+          return TrackScreen();
+        },
+        transitionDuration: Duration.zero,
+        reverseTransitionDuration: Duration.zero,
+      ),
+    );
+  }
+
+  Future<void> _saveExerciseProgressToFirestore(
+    int numSets,
+    int numReps,
+    double numWeights,
+    int mins,
+    String muscleGroup,
+  ) async {
+    // add backend logic here and rmv print after
+    print('4pls: $numSets, $numReps, $numWeights, $muscleGroup,');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Back",
+              style: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w400,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_circle_left_rounded,
+            color: purpleColor,
+            size: 30,
+          ),
+        ),
+        leadingWidth: 30.w,
+      ),
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset(
+                    widget.exercise.imagePath,
+                    height: 150.h,
+                    width: 320.w,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                SizedBox(
+                  height: 12.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      widget.exercise.name,
+                      style: TextStyle(
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 4.h,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Primary muscle group: ",
+                      style: TextStyle(
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.w200,
+                          color: Colors.white),
+                    ),
+                    SizedBox(width: 3.w),
+                    Text(
+                      widget.categoryName,
+                      style: TextStyle(
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.w200,
+                          color: Colors.white),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 29.h,
+                ),
+                if (widget.exercise.isNeedTimer)
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 2.h,
+                      ),
+                      buildRow(
+                        label: "Number of Minutes :",
+                        controller: weightController,
+                      ),
+                      SizedBox(height: 70.h),
+                    ],
+                  ),
+                SizedBox(
+                  height: 2.h,
+                ),
+                if (!widget.exercise.isNeedTimer)
+                  Column(
+                    children: [
+                      buildRow(
+                        label: "Number of Sets:",
+                        controller: setsController,
+                      ),
+                      buildRow(
+                        label: "Number of Reps:",
+                        controller: repsController,
+                      ),
+                    ],
+                  ),
+                SizedBox(
+                  height: 2.h,
+                ),
+                if (widget.exercise.isNeedWeights)
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 2.h,
+                      ),
+                      buildRow(
+                        label: "Number of Weight (lbs):",
+                        controller: weightController,
+                      ),
+                    ],
+                  ),
+                SizedBox(height: 175.h),
+                SizedBox(
+                  width: 320.w,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      handleSave();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: purpleColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: EdgeInsets.symmetric(vertical: 12.h),
+                    ),
+                    child: Text(
+                      "Save",
+                      style: TextStyle(
+                          fontSize: 16.sp,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildRow(
+      {required String label, required TextEditingController controller}) {
+    return Row(
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 12.sp,
+            fontWeight: FontWeight.w200,
+            color: Colors.white,
+          ),
+        ),
+        Spacer(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: Icon(Icons.remove_circle_outline),
+              iconSize: 30,
+              color: purpleColor,
+              onPressed: () {
+                decrement(controller);
+              },
+            ),
+            SizedBox(width: 5.w),
+            Container(
+              width: 78.w,
+              height: 30.h,
+              child: TextFormField(
+                controller: controller,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.zero,
+                  isDense: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white.withOpacity(0.1),
+                ),
+              ),
+            ),
+            SizedBox(width: 5.w),
+            IconButton(
+              icon: Icon(Icons.add_circle_outline),
+              iconSize: 30,
+              color: purpleColor,
+              onPressed: () {
+                increment(controller);
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}

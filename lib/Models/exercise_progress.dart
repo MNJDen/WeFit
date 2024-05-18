eb4194fb2b58b0ea539c8c4a07a339a3cead17a8
“jlexm” <jamaravilla@gbox.adnu.edu.ph>
2024-05-18 19:31:58 +0800

    Models: Add Exercise Progess

---
+++ lib/Models/exercise_progress.dart
@@ -0,0 +1,19 @@
+import 'package:itec303/Models/exercise_item.dart';
+
+class ExerciseProgress {
+  int numSets;
+  int numReps;
+  double numWeights;
+  int mins;
+  ExerciseItem exercise;
+  String muscleGroup;
+
+  ExerciseProgress({
+    required this.numSets,
+    required this.numReps,
+    required this.mins,
+    required this.muscleGroup,
+    required this.exercise,
+    this.numWeights = 0,
+  });
+}
